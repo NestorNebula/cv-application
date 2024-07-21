8267b99cc5e7280598923a34a02e960cd4c983bd
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-07-21 11:19:20 +0200

    feat: Section return Form/List based on type prop

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,6 +1,20 @@
-import Input from './Input';
+import Form from './Form';
 import List from './List';
 
-function Section({ type, cvsubmit = false }) {}
+function Section({ type, cvsubmit = false }) {
+  if (type === 'informations' || type === 'aboutme') {
+    return (
+      <section className={type}>
+        <Form type={type} />
+      </section>
+    );
+  } else {
+    return (
+      <section className={type}>
+        <List type={type} />
+      </section>
+    );
+  }
+}
 
 export default Section;

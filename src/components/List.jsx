192cfab175814d5187beca0957a10ff94f65edb9
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-07-22 11:39:16 +0200

    refactor: change List to works for all types

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -3,43 +3,41 @@ import Form from './Form';
 import { v4 as uuid } from 'uuid';
 
 function List({ type, cvsubmit = false }) {
-  if (type === 'edexperience') {
-    const [expList, setExp] = useState([
+  const [list, setList] = useState([
+    {
+      id: uuid(),
+      content: <Form type={type} />,
+    },
+    {
+      id: uuid(),
+      content: <Form type={type} />,
+    },
+    {
+      id: uuid(),
+      content: <Form type={type} />,
+    },
+  ]);
+
+  function updateList() {
+    setList([
+      ...list,
       {
         id: uuid(),
-        content: <Form type="edexperience" />,
-      },
-      {
-        id: uuid(),
-        content: <Form type="edexperience" />,
-      },
-      {
-        id: uuid(),
-        content: <Form type="edexperience" />,
+        content: <Form type={type} />,
       },
     ]);
-
-    function updateExp() {
-      setExp([
-        ...expList,
-        {
-          id: uuid(),
-          content: <Form type="edexperience" />,
-        },
-      ]);
-    }
-
-    return (
-      <>
-        <ul>
-          {expList.map((exp) => (
-            <li key={exp.id}>{exp.content}</li>
-          ))}
-        </ul>
-        <button onClick={updateExp}>Add</button>
-      </>
-    );
   }
+
+  return (
+    <>
+      <ul>
+        {list.map((li) => (
+          <li key={li.id}>{li.content}</li>
+        ))}
+      </ul>
+      <button onClick={updateList}>Add</button>
+    </>
+  );
 }
 
 export default List;

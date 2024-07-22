05eb347cdd6aa01d4d14e20890ae8958eab2235a
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-07-22 17:14:20 +0200

    feat: add state for submit buttons

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,19 +1,25 @@
+import { useState } from 'react';
 import Form from './Form';
 import List from './List';
 
 function Section({ type, cvsubmit = false }) {
+  const [submit, setSubmit] = useState(true);
+
+  function updateSubmit() {
+    setSubmit(!submit);
+  }
   if (type === 'informations' || type === 'aboutme') {
     return (
       <section className={type}>
         <Form type={type} />
-        <button>Submit</button>
+        <button onClick={updateSubmit}>Submit</button>
       </section>
     );
   } else {
     return (
       <section className={type}>
         <List type={type} />
-        <button>Submit</button>
+        <button onClick={updateSubmit}>Submit</button>
       </section>
     );
   }

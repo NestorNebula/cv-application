72f58daa8b29f6458d230ae64d450a2e69d0eae0
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-07-22 11:16:01 +0200

    feat: add expList and button to add an exp

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,14 +1,45 @@
 import { useState } from 'react';
 import Form from './Form';
+import { v4 as uuid } from 'uuid';
 
 function List({ type, cvsubmit = false }) {
   if (type === 'edexperience') {
+    const [exp, setExp] = useState(3);
+
+    function updateExp() {
+      setExp(exp + 1);
+      expList.push(expForm);
+    }
+
+    let expList = [
+      {
+        id: uuid(),
+        content: <Form type="edexperience" />,
+      },
+      {
+        id: uuid(),
+        content: <Form type="edexperience" />,
+      },
+      {
+        id: uuid(),
+        content: <Form type="edexperience" />,
+      },
+    ];
+
+    const expForm = {
+      id: uuid(),
+      content: <Form type="edexperience" />,
+    };
+
     return (
-      <ul>
-        <Form type={type} />
-        <Form type={type} />
-        <Form type={type} />
-      </ul>
+      <>
+        <ul>
+          {expList.map((exp) => (
+            <li key={exp.id}>{exp.content}</li>
+          ))}
+        </ul>
+        <button onClick={updateExp}>Add</button>
+      </>
     );
   }
 }

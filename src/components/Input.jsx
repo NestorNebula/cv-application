2d95f6344533a2c7d4923cceef38b8ea2785a74b
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-07-22 10:01:50 +0200

    feat: add Input props and return value

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,3 +1,10 @@
-function Input({ content, status }) {}
+function Input({ content, value, update, status }) {
+  return (
+    <>
+      <label htmlFor={content}>{content}: </label>
+      <input id={content} value={value} onChange={update}></input>
+    </>
+  );
+}
 
 export default Input;

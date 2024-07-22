37f3a8a5089363396e2ff4ccd1947e67afdfccd5
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-07-22 10:01:19 +0200

    feat: add Form content for informations

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,3 +1,31 @@
-function Form({ type, cvsubmit = false }) {}
+import { useState } from 'react';
+import Input from './Input';
+
+function Form({ type, cvsubmit = false }) {
+  if (type === 'informations') {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+
+    function updateName(e) {
+      setName(e.target.value);
+    }
+
+    function updateEmail(e) {
+      setEmail(e.target.value);
+    }
+
+    function updatePhone(e) {
+      setPhone(e.target.value);
+    }
+    return (
+      <form>
+        <Input content="Name" value={name} update={updateName} />
+        <Input content="Email" value={email} update={updateEmail} />
+        <Input content="Phone number" value={phone} update={updatePhone} />
+      </form>
+    );
+  }
+}
 
 export default Form;

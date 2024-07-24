e5a04bc9a45b31055dfb06442e11225d8030e9d5
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-07-24 09:19:48 +0200

    style: change input styles

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -5,7 +5,7 @@ function Input({ content, label, value, update, className, status }) {
     <div className="input" id={className + 'input'}>
       <input id={content} value={value} onChange={update}></input>
       <label className="inputlabel" htmlFor={content}>
-        {label + ': '}
+        {label}
       </label>
     </div>
   );

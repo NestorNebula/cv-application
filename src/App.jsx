85f5535280e0235be69a44ffa50b79f40e08793e
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-07-26 09:44:19 +0200

    refactor: add credits to the page

--- src/App.jsx
+++ src/App.jsx
@@ -10,27 +10,49 @@ function App() {
     setCvSubmit(!cvsubmit);
   }
   return (
-    <div className="cv">
-      <main>
-        <div className="aside">
-          <Section type="informations" cvsubmit={cvsubmit} />
-          <Section type="skills" cvsubmit={cvsubmit} />
-        </div>
-        <div className="mainsection">
-          <Section type="aboutme" cvsubmit={cvsubmit} />
-          <Section type="education" cvsubmit={cvsubmit} />
-          <Section type="experience" cvsubmit={cvsubmit} />
-        </div>
-        {cvsubmit ? null : (
-          <div className="cvbuttons">
-            <Dialog />
-            <button className="cvsubmitbtn" onClick={updateCvSubmit}>
-              Submit CV
-            </button>
+    <>
+      <div className="cv">
+        <main>
+          <div className="aside">
+            <Section type="informations" cvsubmit={cvsubmit} />
+            <Section type="skills" cvsubmit={cvsubmit} />
           </div>
-        )}
-      </main>
-    </div>
+          <div className="mainsection">
+            <Section type="aboutme" cvsubmit={cvsubmit} />
+            <Section type="education" cvsubmit={cvsubmit} />
+            <Section type="experience" cvsubmit={cvsubmit} />
+          </div>
+          {cvsubmit ? null : (
+            <div className="cvbuttons">
+              <Dialog />
+              <button className="cvsubmitbtn" onClick={updateCvSubmit}>
+                Submit CV
+              </button>
+            </div>
+          )}
+        </main>
+      </div>
+      <div className="credits">
+        <a href="https://www.flaticon.com/free-icons/tick" title="tick icons">
+          Tick icons created by Roundicons - Flaticon
+        </a>
+        <a href="https://www.flaticon.com/free-icons/plus" title="plus icons">
+          Plus icons created by kliwir art - Flaticon
+        </a>
+        <a
+          href="https://www.flaticon.com/free-icons/subtract"
+          title="subtract icons"
+        >
+          Subtract icons created by Fathema Khanom - Flaticon
+        </a>
+        <a
+          href="https://www.flaticon.com/free-icons/pencil"
+          title="pencil icons"
+        >
+          Pencil icons created by riajulislam - Flaticon
+        </a>
+      </div>
+    </>
   );
 }
 
